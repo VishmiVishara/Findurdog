4fb33d4a6924a962ace8d2fbe5a2aea03ee98aab
Vishmi <vishivishara@gmail.com>
2020-02-23 10:02:24 +0530

    first commit

---
+++ app/src/main/java/com/iit/findyourdog/IdentifyTheBreedActivity.java
@@ -0,0 +1,14 @@
+package com.iit.findyourdog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IdentifyTheBreedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_identify_the_breed);
+    }
+}
